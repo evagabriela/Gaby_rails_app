12f87dbf257e4b3eb9f4cb7b8db2b9022e91e94c
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-05 15:42:54 -0800

    add route create skeleton for signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_byemail(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Sign the user in and redirect to the user's show page
+    else
+      #creae an error message and re-render the signin form
+    end
   end
 
   def destroy
