6fd144fc97e34b0272deb58d7008db302b7f9196
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-06 15:12:20 -0800

    add curent_user sign in methods in session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token #rails automatically set the expiration to 20.years.from_now
     self.current_user = user
   end
+
+  def current_user=(user)
+    @current_user = user
+  end
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])  #prevent nil
+  end
 end
