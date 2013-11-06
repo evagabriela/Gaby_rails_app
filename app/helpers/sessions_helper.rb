2285186e37f06cb7119fa22503f46be22db99545
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-05 16:40:37 -0800

    add cookies to be set to 20 years expiration under sign_in method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token #rails automatically set the expiration to 20.years.from_now
+    self.current_user = user
+  end
 end
