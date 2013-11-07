1c7709047045e22f84a7035897d20913b0e06dd6
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-06 16:14:37 -0800

    add js boostrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination' 
       render 'new'
@@ -14,6 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    
+    sign_out
+    redirect_to root_url
   end
 end
