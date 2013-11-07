cf93c70d858b9b84d74990aad87b9c409b4989c4
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-07 13:12:42 -0800

    add new edit and update route for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+      @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      #handle a successful update
+    else
+      render 'edit'
+    end
+  end
 end
