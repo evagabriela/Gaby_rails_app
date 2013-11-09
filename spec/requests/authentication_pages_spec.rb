7ac552e289f8722701b10d4705b744c8955de008
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-08 18:01:55 -0800

    add testing for admin and for delete links

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -98,6 +98,18 @@ describe "Authentication" do
           end
         end
       end
+
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
+
+        before { sign_in non_admin }
+
+        describe "submitting a DELETE request to the Users#destroy action" do
+          before { delete user_path}
+          specify { response.should redirect_to(root_url)}
+        end
+      end
     end
   end
 end
