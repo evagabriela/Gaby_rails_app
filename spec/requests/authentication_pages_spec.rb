d69066faae81d5a67e9e27db84af1e305d74abc3
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-08 16:03:47 -0800

    add testing for user index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,16 +28,15 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user}
 
       it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users',   href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user))}
       it { should have_link('Sign out', href: signout_path) }
+      
       it { should_not have_link('Sign in', href: signin_path) }
 
        describe "followed by signout" do
@@ -93,6 +92,10 @@ describe "Authentication" do
             specify { response.should redirect_to(root_url) }
           end
 
+          describe "visiting the user index" do
+            before { visit user_path }
+            it { should have_selector('title', text: 'Sign in')}
+          end
         end
       end
     end
