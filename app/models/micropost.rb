ea5d00e774006426681a7c63a7566afe64e16ce3
Eva Zamudio <gabriela.osu@gmail.com>
2013-11-10 19:06:49 -0800

    add testing for blank or invalid cases and also add validation for length max 140

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
 
+  validates :content, presence: true, length: { maximum: 140}
   validates :user_id, presence: true
 
   default_scope order: 'microposts.create_at DESC'
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -17,6 +17,16 @@ describe Micropost do
     it { should_not be_valid }
   end
 
+  describe "with blank content" do
+    before { @micropost.content = " "}
+    it { should_not be_valid}
+  end
+
+  describe "with content that is too long" do
+    before { @micropost.content = "a" * 141 }
+    it { should_not be_valid}
+  end
+
   describe "accessible attributes" do
     it "should not allow access to user_id" do
       expect do
